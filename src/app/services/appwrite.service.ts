288bb72deffad8610c293caea5b9b49bb5403841
Simon Esders <simonesders2003@gmail.com>
2024-01-03 00:49:48 +0100

    setup Account class

--- src/app/services/appwrite.service.ts
+++ src/app/services/appwrite.service.ts
@@ -7,10 +7,12 @@ import { Client, Account } from 'appwrite';
 export class AppwriteService {
 
   client = new Client();
+  account!: Account;
 
   constructor() {
     this.client
       .setEndpoint('https://cloud.appwrite.io/v1')
       .setProject('65949b66b5d7b911fa48');
+    this.account = new Account(this.client);
   }
 }
