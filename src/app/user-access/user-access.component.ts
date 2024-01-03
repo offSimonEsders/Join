b506c5c06d77a3150a7e3b680d6ea487ede1fe91
Simon Esders <simonesders2003@gmail.com>
2024-01-03 07:55:31 +0100

    logo animation

--- src/app/user-access/user-access.component.ts
+++ src/app/user-access/user-access.component.ts
@@ -1,5 +1,5 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { AfterViewInit, Component, ElementRef, ViewChild } from '@angular/core';
+import { ActivatedRoute, Router, RouterOutlet } from '@angular/router';
 import { LoginComponent } from "./login/login.component";
 
 @Component({
@@ -9,6 +9,45 @@ import { LoginComponent } from "./login/login.component";
     styleUrl: './user-access.component.scss',
     imports: [RouterOutlet, LoginComponent]
 })
-export class UserAccessComponent {
+export class UserAccessComponent implements AfterViewInit {
+    @ViewChild('bigLogo') bigLogo?: ElementRef;
+    @ViewChild('logoContainer') logoContainer?: ElementRef
+    @ViewChild('disappearingcontainer') disappearingcontainer?: ElementRef;
+
+    constructor(private router: Router) {
+
+    }
+    ngAfterViewInit(): void {
+        const bigLogoElement = this.bigLogo?.nativeElement;
+        const logoContainer = this.logoContainer?.nativeElement;
+        const disappearingcontainer = this.disappearingcontainer?.nativeElement;
+        if (bigLogoElement && window.innerWidth <= 800) {
+            bigLogoElement.style.left = ((window.innerWidth - logoContainer.offsetWidth) / 2) + 'px';
+            bigLogoElement.style.top = '37px';
+            if(this.router.url == '/registration') {
+                bigLogoElement.classList.remove('animate-join-logo');
+                disappearingcontainer.style.display = 'none';
+                return;
+            }
+            bigLogoElement.classList.add('animate-join-logo');
+            disappearingcontainer.style.display = 'flex';
+            bigLogoElement.style.backgroundImage = "url('../../assets/Logos/Join_Logo_Light.svg')";
+        }
+        window.addEventListener('resize', () => {
+            if (bigLogoElement && window.innerWidth <= 800) {
+                bigLogoElement.style.left = ((window.innerWidth - logoContainer.offsetWidth) / 2) + 'px';
+                bigLogoElement.style.top = '37px';
+            }
+        })
+        bigLogoElement.addEventListener('animationend', () => {
+            if (bigLogoElement && window.innerWidth <= 800) {
+                bigLogoElement.style.left = ((window.innerWidth - logoContainer.offsetWidth) / 2) + 'px';
+                bigLogoElement.style.top = '37px';
+                bigLogoElement.style.backgroundImage = "url('../../assets/Logos/Join_Logo_Dark.svg')";
+                bigLogoElement.classList.remove('animate-join-logo');
+                disappearingcontainer.style.display = 'none';
+            }
+        })
+    }
 
 }
