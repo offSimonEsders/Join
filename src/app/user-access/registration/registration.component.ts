f47253182fba3d79f1cb212a557028db3e75d9f4
Simon Esders <simonesders2003@gmail.com>
2024-01-03 03:59:59 +0100

    registration html and css

--- src/app/user-access/registration/registration.component.ts
+++ src/app/user-access/registration/registration.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-registration',
@@ -8,5 +9,9 @@ import { Component } from '@angular/core';
   styleUrl: './registration.component.scss'
 })
 export class RegistrationComponent {
+
+  constructor(public router: Router) {
+    
+  }
 
 }
