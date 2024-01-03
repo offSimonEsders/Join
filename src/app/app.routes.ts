c19d1ad861c28b9dc25c41e104c5e01b228a22af
Simon Esders <simonesders2003@gmail.com>
2024-01-03 03:22:28 +0100

    legal container

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,6 +2,8 @@ import { Routes } from '@angular/router';
 import { UserAccessComponent } from './user-access/user-access.component';
 import { RegistrationComponent } from './user-access/registration/registration.component';
 import { LoginComponent } from './user-access/login/login.component';
+import { PrivacyPolicyComponent } from './privacy-policy/privacy-policy.component';
+import { LegalNoticeComponent } from './legal-notice/legal-notice.component';
 
 export const routes: Routes = [
     {
@@ -9,5 +11,7 @@ export const routes: Routes = [
             { path: '', component: LoginComponent },
             { path: 'registration', component: RegistrationComponent } // Fügen Sie die neue Kindroute hinzu
         ]
-    }
+    },
+    { path: 'privacypolicy', component: PrivacyPolicyComponent },
+    { path: 'legalnotice', component: LegalNoticeComponent }
 ];
