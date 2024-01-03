30be70ec9023d73df7c90d776fe459b6f12a4ac8
Simon Esders <simonesders2003@gmail.com>
2024-01-03 01:10:16 +0100

    appwrite service signup

--- src/app/services/appwrite.service.ts
+++ src/app/services/appwrite.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { Client, Account } from 'appwrite';
+import { Client, Account, ID } from 'appwrite';
 
 @Injectable({
   providedIn: 'root'
@@ -15,4 +15,20 @@ export class AppwriteService {
       .setProject('65949b66b5d7b911fa48');
     this.account = new Account(this.client);
   }
+
+  /**
+   * @param {string} newUserEmail - Email of new user
+   * @param {string} newUserPassword - Password for email of new user
+   * @param {string} newUserName - Name of new user
+   */
+  appwriteSignUp(newUserEmail: string, newUserPassword: string, newUserName: string) {
+    this.account.create(ID.unique(), newUserEmail, newUserPassword, newUserName)
+      .then((response) => {
+        console.log(response);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }
+
 }
