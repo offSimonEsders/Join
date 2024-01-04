f2dcf8bba0d91e1362710b7970ecad29ee2ac462
Simon Esders <simonesders2003@gmail.com>
2024-01-04 01:54:45 +0100

    header and section style

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,12 +1,14 @@
 import { Component } from '@angular/core';
 import { SidebarComponent } from "./sidebar/sidebar.component";
+import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from "./header/header.component";
 
 @Component({
     selector: 'app-home',
     standalone: true,
     templateUrl: './home.component.html',
     styleUrl: './home.component.scss',
-    imports: [SidebarComponent]
+    imports: [SidebarComponent, RouterOutlet, HeaderComponent]
 })
 export class HomeComponent {
 
