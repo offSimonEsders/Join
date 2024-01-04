34216c2b465cb792c647a1d49bd2d11f5e0eb04a
Simon Esders <simonesders2003@gmail.com>
2024-01-04 21:18:31 +0100

    task component

--- src/app/home/sections/board/board.component.ts
+++ src/app/home/sections/board/board.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
+import { SingleTaskComponent } from "./single-task/single-task.component";
 
 @Component({
-  selector: 'app-board',
-  standalone: true,
-  imports: [],
-  templateUrl: './board.component.html',
-  styleUrl: './board.component.scss'
+    selector: 'app-board',
+    standalone: true,
+    templateUrl: './board.component.html',
+    styleUrl: './board.component.scss',
+    imports: [SingleTaskComponent]
 })
 export class BoardComponent {
 
