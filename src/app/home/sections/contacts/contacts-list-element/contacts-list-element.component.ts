5245a34f6baf945b2de8912e8eee6b25162479be
Simon Esders <simonesders2003@gmail.com>
2024-01-05 21:59:43 +0100

    generate contacts

--- src/app/home/sections/contacts/contacts-list-element/contacts-list-element.component.ts
+++ src/app/home/sections/contacts/contacts-list-element/contacts-list-element.component.ts
@@ -1,13 +1,19 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-contacts-list-element',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './contacts-list-element.component.html',
   styleUrls: ['./contacts-list-element.component.scss', '../../../extra-styles/contact-icon-color.scss']
 })
-export class ContactsListElementComponent {
+export class ContactsListElementComponent implements OnInit {
   @Input() contact: any;
+  @Input() letter?: string;
+
+  ngOnInit(): void {
+    console.log(this.contact.name[0].toUpperCase())
+  }
 
 }
--- src/app/home/sections/contacts/contacts.component.ts
+++ src/app/home/sections/contacts/contacts.component.ts
@@ -13,6 +13,8 @@ import { ContactsListElementComponent } from "./contacts-list-element/contacts-l
 export class ContactsComponent implements OnInit {
     contacts: any;
 
+    letters: string[] = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
+
     constructor(private appwriteService: AppwriteService) {
 
     }
@@ -20,6 +22,20 @@ export class ContactsComponent implements OnInit {
     async ngOnInit() {
         this.contacts = (await this.appwriteService.getContacts());
         console.log(this.contacts)
+    }
+
+    returnIfNameWithLetterexist(letter: string) {
+        try {
+            for (let c of this.contacts) {
+                if (c.name[0].toUpperCase() == letter) {
+                    return true;
+                }
+            }
+            return false;
+        }
+        catch {
+            return;
+        }
     }
 
 }
