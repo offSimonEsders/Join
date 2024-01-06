111d59acc3fa7557aba2412aa48830cb4ab9dc91
Simon Esders <simonesders2003@gmail.com>
2024-01-06 21:36:42 +0100

    validate form inputs

--- src/app/home/sections/contacts/create-edit-contact/create-edit-contact.component.ts
+++ src/app/home/sections/contacts/create-edit-contact/create-edit-contact.component.ts
@@ -15,6 +15,7 @@ export class CreateEditContactComponent {
   @Output() closeWindowEvent = new EventEmitter<any>();
   @Output() contactEdited = new EventEmitter<Contact>();
   @Output() deleteContactEvent = new EventEmitter<string>();
+  mailregex: RegExp = /[a-z0-9]+@[a-z]+\.[a-z]/;
 
   constructor(private appwriteService: AppwriteService) {
 
@@ -29,9 +30,51 @@ export class CreateEditContactComponent {
     element.value = element.value.replace(/[^+0-9 ]/g, "");
   }
 
-  async createNewContact(event: Event, name: string, mail: string, phone: string) {
+  checkInputs(name: string, email: string, phone: string, namecontainer: HTMLDivElement, emailcontainer: HTMLDivElement, phonecontainer: HTMLDivElement) {
+    let stop = false;
+    stop = this.checkName(name, namecontainer);
+    stop = this.checkEmail(email, emailcontainer);
+    stop = this.checkPhone(phone, phonecontainer);
+    if (stop) {
+      return true;
+    }
+    return false;
+  }
+
+  checkName(name: string, namecontainer: HTMLDivElement,) {
+    if (name.length <= 0) {
+      namecontainer.classList.add('name-wrong-input');
+      return true;
+    }
+    return false;
+  }
+
+  checkEmail(email: string, emailcontainer: HTMLDivElement) {
+    if (!this.mailregex.test(email)) {
+      emailcontainer.classList.add('email-wrong-input');
+      return true;
+    }
+    return false;
+  }
+
+  checkPhone(phone: string, phonecontainer: HTMLDivElement) {
+    if (phone.length <= 0) {
+      phonecontainer.classList.add('phone-wrong-input');
+      return true;
+    }
+    return false;
+  }
+
+  resetContainer(container: HTMLDivElement, classToRemove: string) {
+    container.classList.remove(classToRemove);
+  }
+
+  async createNewContact(event: Event, name: string, email: string, phone: string, namecontainer: HTMLDivElement, mailcontainer: HTMLDivElement, phonecontainer: HTMLDivElement) {
     event.preventDefault();
-    const newContact = new Contact(name, mail, phone, this.getInitials(name), this.getVariant());
+    if (this.checkInputs(name, email, phone, namecontainer, mailcontainer, phonecontainer)) {
+      return;
+    }
+    const newContact = new Contact(name, email, phone, this.getInitials(name), this.getVariant());
     this.closeCreateEditWindow(event);
     await this.appwriteService.createContact(newContact);
   }
@@ -55,8 +98,11 @@ export class CreateEditContactComponent {
     return 'variant' + Math.round(randInt * 15);
   }
 
-  updateContact(event: Event, name: string, email: string, phone: string) {
+  updateContact(event: Event, name: string, email: string, phone: string, namecontainer: HTMLDivElement, mailcontainer: HTMLDivElement, phonecontainer: HTMLDivElement) {
     event.preventDefault();
+    if (this.checkInputs(name, email, phone, namecontainer, mailcontainer, phonecontainer)) {
+      return;
+    }
     this.changeConactData(name, email, phone);
     this.contactEdited.emit(this.editcontact);
     this.appwriteService.updateContact(this.editcontact);
