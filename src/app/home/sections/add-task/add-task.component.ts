bf67adcaec9c07dd4d8c98ce9ecd99b01f308ce2
Simon Esders <simonesders2003@gmail.com>
2024-01-07 02:08:05 +0100

    add task layout and style

--- src/app/home/sections/add-task/add-task.component.ts
+++ src/app/home/sections/add-task/add-task.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-add-task',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './add-task.component.html',
   styleUrl: './add-task.component.scss'
 })
