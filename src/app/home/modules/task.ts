13a7f98025c9039312afc4a4526c83b8d4abf789
Simon Esders <simonesders2003@gmail.com>
2024-01-10 17:49:24 +0100

    task class

--- src/app/home/modules/task.ts
+++ src/app/home/modules/task.ts
@@ -1,3 +1,24 @@
 export class Task {
-    
+    title: string;
+    description?: string;
+    assignedConacts?: string[];
+    duedate: string;
+    prio: string;
+    category: string;
+    subtasks?: string[];
+    subtasksdone?: boolean[];
+    state: string;
+
+    constructor(title: string, description: string, assignedConacts: string[], duedate: string, prio: string, category: string, subtasks: string[], state: string) {
+        this.title = title;
+        this.description = description;
+        this.assignedConacts = assignedConacts;
+        this.duedate = duedate;
+        this.prio = prio;
+        this.category = category;
+        this.subtasks = subtasks;
+        this.subtasksdone = subtasks.map(() => false);
+        this.state = state;
+    }
+
 }
