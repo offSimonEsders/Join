f6a5c470801a5693d483e1501e447c7488e5bbfa
Simon Esders <simonesders2003@gmail.com>
2024-01-12 17:52:03 +0100

    get nearest date of urgent tasks

--- src/app/home/sections/summary/summary.component.ts
+++ src/app/home/sections/summary/summary.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { AppwriteService } from '../../../services/appwrite.service';
+import { Task } from '../../modules/task';
 
 @Component({
   selector: 'app-summary',
@@ -11,13 +12,17 @@ import { AppwriteService } from '../../../services/appwrite.service';
 })
 export class SummaryComponent implements OnInit {
   userName?: string;
+  tasks?: Task[];
+  monthNames: string[] = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
 
-  constructor (private router: Router, private appwriteService: AppwriteService) {
-    this.getGreeting();
+  constructor(private router: Router, private appwriteService: AppwriteService) {
+
   }
 
   async ngOnInit() {
+    this.tasks = await this.appwriteService.getTasks() as unknown as Task[];
     this.userName = await this.appwriteService.getUserName();
+    console.log(this.tasks)
   }
 
   goToBoard() {
@@ -33,6 +38,39 @@ export class SummaryComponent implements OnInit {
     } else {
       return 'Good evening';
     }
+  }
+
+  getTasksInState(state: string) {
+    let count = 0;
+    this.tasks?.forEach((t: Task) => {
+      if (t.state == state) {
+        count++;
+      }
+    });
+    return count;
+  }
+
+  getUrgentTasks() {
+    let count = 0;
+    this.tasks?.forEach((t: Task) => {
+      if (t.prio == 'Urgent') {
+        count++;
+      }
+    });
+    return count;
+  }
+
+  getNearestUrgentTask() {
+    let dates = this.tasks?.map(task => parseInt(task.duedate.replace("-", "").replace("-", "")));
+    if (dates) {
+      let nearestDate = Math.min(...dates).toString();
+      let year = parseInt(nearestDate.substr(0, 4));
+      let month = parseInt(nearestDate.substr(4, 2)) - 1; // Months are 0-indexed in JavaScript
+      let day = parseInt(nearestDate.substr(6, 2));
+      return `${this.monthNames[month]} ${day}, ${year}`
+    }
+    console.log(dates)
+    return 'MM,DD,YYYY'
   }
 
 }
