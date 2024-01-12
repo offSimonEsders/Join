a244bcf4f3b9689a76189235e5e4668c877135d3
Simon Esders <simonesders2003@gmail.com>
2024-01-12 14:43:21 +0100

    open close popup and logout

--- src/app/home/header/header.component.ts
+++ src/app/home/header/header.component.ts
@@ -1,12 +1,38 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { AppwriteService } from '../../services/appwrite.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss'
 })
 export class HeaderComponent {
+  showPopup: boolean = false;
+
+  constructor(private appwriteService: AppwriteService, private router: Router) {
+    window.addEventListener('click', (event) => {
+      this.closePopup(event);
+    });
+  }
+
+  changeShowPopup() {
+    this.showPopup = !this.showPopup;
+  }
+
+  closePopup(event: Event) {
+    const targetElement = event.target as HTMLElement;
+    if(!targetElement.classList.contains('popup')) {
+      this.showPopup = false;
+    }
+  }
+
+  logOut() {
+    this.appwriteService.appwriteLogOut();
+    this.router.navigate(['/']);
+  }
 
 }
